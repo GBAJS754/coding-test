468cbe57661b1e8309ce4d2a3e6f56c0963e9e76
Kimdaeun <104294861+GBAJS754@users.noreply.github.com>
2024-02-04 00:25:40 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+select ID, NAME, HOST_ID
+from PLACES
+where HOST_ID in (SELECT HOST_ID
+                    from PLACES 
+                    group by HOST_ID
+                    having count(HOST_ID) >= 2)
+order by ID 
