8bc6f7afa841af0f797ece92180c03471950faa8
Kimdaeun <104294861+GBAJS754@users.noreply.github.com>
2024-02-14 15:03:39 +0900

    [level 2] Title: 피로도, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -2,19 +2,22 @@ function solution(k, dungeons) {
     const len = dungeons.length
     const temp = []
     const check = new Array(len).fill(0)
-    const recursion =(n)=>{
+    let answer = 0;
+    const recursion =(n, hp)=>{
         if(n === len) {
             return
         }
         for(let i=0; i<len; i++){
-            if(check[i] === 0){
+            if(check[i] === 0 && hp >= dungeons[i][0]) {
             check[i] = 1
             temp.push(dungeons[i]) 
-            recursion(n+1)
+            answer = Math.max(answer, temp.length)
+            recursion(n+1, hp-dungeons[i][1])
             check[i] = 0
             temp.pop()   
             }
         }
     }
-    recursion(0)
+    recursion(0, k)
+    return answer
 }
