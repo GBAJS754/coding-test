7f6dde055bcff7b97d8422a17e0a0b8d8a219680
Kimdaeun <104294861+GBAJS754@users.noreply.github.com>
2024-02-19 18:30:18 +0900

    [level 2] Title: n^2 배열 자르기, Time: 5.95 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,15 +1,18 @@
 function solution(n, left, right) {
-    const answer = []
-    for(let i=left; i<=right; i++){   
-        const a = Math.floor(i/n) // 열
-        const b = Math.floor(i%n) // 행
-        if(a <= b){
-            answer.push(b+1)
-        }else{
-            const c = Math.abs(a-b)
-            answer.push(b+c+1)
+    const dp =[]
+    dp[1] = [[1]]
+    dp[2] = [[1,2],[2,2]]
+    dp[3] = [[1,2,3], [2,2,3], [3,3,3]]
+
+    for(let i=4; i<=n; i++){
+        dp[i] = dp[i-1].map(v=>[...v])
+        for(let j=0; j<dp[i].length; j++){
+            dp[i][j].push(i)
         }
+        dp[i].push(String(i).repeat(4).split("").map(Number))
     }
-    return answer
     
+    const answer = dp[n].map((v)=>v.join("")).join("")
+
+    return answer.slice(left,right+1).split("").map(Number)
 }
