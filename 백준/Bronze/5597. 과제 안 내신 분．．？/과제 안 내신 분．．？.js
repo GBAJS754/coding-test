2eff74e84fb92089a71fb3654ec284666167fce4
Kimdaeun <104294861+GBAJS754@users.noreply.github.com>
2024-07-11 09:19:00 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 96 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.js
@@ -0,0 +1,15 @@
+let input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+const answer = [];
+
+for (let i = 1; i <= 30; i++) {
+  if (!input.includes(i)) {
+    answer.push(i);
+  }
+}
+
+console.log(answer.sort((a, b) => a - b).join("\n"));
